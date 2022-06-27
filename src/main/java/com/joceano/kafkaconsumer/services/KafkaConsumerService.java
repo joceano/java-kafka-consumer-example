70c1d74e621786d0aa91cc82edb705aa6f890569
07466621902 <joceano.borba.ext@panonline.com.br>
2022-06-27 16:27:49 -0300

    init project

---
+++ src/main/java/com/joceano/kafkaconsumer/services/KafkaConsumerService.java
@@ -0,0 +1,19 @@
+package com.joceano.kafkaconsumer.services;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @Value("${topic.name.consumer")
+    private String topicName;
+
+    @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
+    public void consume(ConsumerRecord<String, String> payload){
+        System.out.println(payload);
+    }
+
+}
