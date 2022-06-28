5f8c81743b748547940975af41dbd72bda480fb9
07466621902 <joceano.borba.ext@panonline.com.br>
2022-06-27 21:58:16 -0300

    consumer object pedido

---
+++ src/main/java/com/joceano/kafkaconsumer/models/enums/PedidoStatus.java
@@ -0,0 +1,8 @@
+package com.joceano.kafkaconsumer.models.enums;
+
+public enum PedidoStatus {
+
+    PENDENTE,
+    APROVADO
+
+}
--- src/main/java/com/joceano/kafkaconsumer/services/KafkaConsumerService.java
+++ src/main/java/com/joceano/kafkaconsumer/services/KafkaConsumerService.java
@@ -1,19 +1,15 @@
 package com.joceano.kafkaconsumer.services;
 
 import com.joceano.kafkaconsumer.models.Pedido;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
 
-    @Value("${topic.name.consumer")
-    private String topicName;
-
-    @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
+    @KafkaListener(topics = "${topic.name.consumer}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(Pedido pedido){
-        System.out.println(pedido);
+        System.out.println(pedido.getStatus().name());
     }
 
 }
